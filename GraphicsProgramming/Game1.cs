cf9114aad7a99598c2cc081d6906c999a8011534
mijkolsmith <mijkolsmith@live.com>
2021-02-04 15:01:19 +0100

    first commit

---
+++ GraphicsProgramming/Game1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class Game1 : Game
+{
+	private GraphicsDeviceManager _graphics;
+	private SpriteBatch _spriteBatch;
+	private Lesson currentLesson;
+
+	public Game1()
+	{
+		_graphics = new GraphicsDeviceManager(this);
+		Content.RootDirectory = "Content";
+		IsMouseVisible = true;
+
+		currentLesson = new LiveLesson1();
+	}
+
+	protected override void Initialize()
+	{
+		// Add your initialization logic here
+		currentLesson.Initialize();
+
+		_graphics.PreferredBackBufferWidth = 1080;
+		_graphics.PreferredBackBufferHeight = 1080;
+		_graphics.ApplyChanges();
+
+		base.Initialize();
+	}
+
+	protected override void LoadContent()
+	{
+		_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+		// Use this.Content to load your game content here
+		currentLesson.LoadContent(Content, _graphics, _spriteBatch);
+	}
+
+	protected override void Update(GameTime gameTime)
+	{
+		if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			Exit();
+
+		// Add your update logic here
+		currentLesson.Update(gameTime);
+		base.Update(gameTime);
+	}
+
+	protected override void Draw(GameTime gameTime)
+	{
+		GraphicsDevice.Clear(Color.CornflowerBlue);
+
+		// Add your drawing code here
+		currentLesson.Draw(gameTime, _graphics, _spriteBatch);
+		base.Draw(gameTime);
+	}
+}
---
+++ GraphicsProgramming/Lesson.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+abstract class Lesson
+{
+	public virtual void Initialize() { }
+	public virtual void LoadContent(ContentManager Content, GraphicsDeviceManager graphics, SpriteBatch spriteBatch) { }
+	public virtual void Update(GameTime gameTime) { }
+	public virtual void Draw(GameTime gameTime, GraphicsDeviceManager graphics, SpriteBatch spriteBatch) { }
+}
